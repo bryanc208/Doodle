45746e9913531110d81cf7c81c95f44f9e8a0320
bryanc208 <bryanc208@gmail.com>
2015-01-12 23:29:18 -0500

    Some updates
    DoodleV1

--- oldfiles/script.js
+++ oldfiles/script.js
@@ -2,7 +2,16 @@ var clickX = new Array();
 var clickY = new Array();
 var clickDrag = new Array();
 var paint;
+var canvasWidth = 480;
+var canvasHeight = 800;
 
+var colorPink = "#cb3594";
+var colorRed = "#F00000";
+var colorBlue = "#0066FF";
+var colorYellow = "#ffcf33";
+var colorBlack = "#181818";
+
+var curColor = colorPink;
 var clickColor = new Array();
 var imageObj = new Image();
 
@@ -10,102 +19,33 @@ var textToFill = '';
 var textYLoc = 600;
 var textUsed = false;
 
-var MAX_WIDTH = 800;
-var MAX_HEIGHT = 600;
 
-var width;
-var height;
-
-var currentStep = 0;
-var lastStep = 0;
-var differences = [];
-var curColor = "#222222"
 
 $(document).ready(function(){
-    $('#colorSelector').css('backgroundColor', curColor);
-    //color picker
-    $('#colorSelector').ColorPicker({
-        color: '#222222',
-        onShow: function (colpkr) {
-            $(colpkr).fadeIn(500);
-            return false;
-        },
-        onHide: function (colpkr) {
-            $(colpkr).fadeOut(500);
-            return false;
-        },
-        onChange: function (hsb, hex, rgb) {
-            curColor = "#" + hex;
-            $('#colorSelector').css('backgroundColor', curColor);
-            $("#draw").prop("checked", true);
-        }
-    });
-    
-    imageObj.onload = function() { 
-        //maintaining aspect ratio
-        width = imageObj.width;
-        height = imageObj.height;
-
-        if (width > height) {
-          if (width > MAX_WIDTH) {
-            height *= MAX_WIDTH / width;
-            width = MAX_WIDTH;
-          }
-        } else {
-          if (height > MAX_HEIGHT) {
-            width *= MAX_HEIGHT / height;
-            height = MAX_HEIGHT;
-          }
-        }
-        canvas.width = width;
-        canvas.height = height;
-        context.drawImage(imageObj, 0, 0, width, height);
-        
-        $('canvas').width = width;
-        $('canvas').height = height;
+    imageObj.onload = function() {
+        context.drawImage(imageObj, 0, 0, canvasWidth, canvasHeight);
     }
     imageObj.src = '';
     
     $('#save').click(function(){
             var dataURL = canvas.toDataURL();
-            $('#canvasImg').src = dataURL;
+            document.getElementById('canvasImg').src = dataURL;
     });
-        
+    
     canvas = document.getElementById('canvas');
     context = canvas.getContext("2d");
     
-        
     $('#textToAdd').keyup(function(){
         textToFill = $('#textToAdd').val();
         redraw();
     });
     
-    
-/*
-    $(document).keyup(function(e){
-        if(e.shiftKey){
-            text = String.fromCharCode(e.keyCode);
-        }else{
-            text = String.fromCharCode(e.keyCode).toLowerCase();
-        }
-        if(e.keyCode === 8){
-            text[text.length]='';
-        }
-        textToFill += text;
-    });
-*/
-
 
     $('#canvas').mousedown(function(e){
         var mouseX = e.pageX - this.offsetLeft;
         var mouseY = e.pageY - this.offsetTop;
-        console.log(mouseX);
-        console.log(mouseY);
 
         paint = true;
-        if(document.getElementById('draw').checked){
-            currentStep = clickX.length;
-        }
         addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
         redraw();
     });
@@ -119,49 +59,42 @@ $(document).ready(function(){
 
     $('#canvas').mouseup(function(e){
         paint = false;
-        if(document.getElementById('draw').checked){
-            lastStep = clickX.length;
-            differences.push(lastStep-currentStep);
-        }   
     });
 
     $('#canvas').mouseleave(function(e){
         paint = false;
-        if(document.getElementById('draw').checked){
-            lastStep = clickX.length;
-            differences.push(lastStep-currentStep);
-        }
     });
     
+    $('#colorRed').click(function() {
+        curColor = colorRed;
+    });
+    $('#colorBlue').click(function() {
+        curColor = colorBlue;
+    });
+    $('#colorYellow').click(function() {
+        curColor = colorYellow;
+    });
+    $('#colorBlack').click(function() {
+        curColor = colorBlack;
+    });
+    $('#colorPink').click(function() {
+        curColor = colorPink;
+    });
     $('#clearButton').click(function() {
         clearCanvas();
     });
-    
-    $('#upload').click(upload);
-    
     document.getElementById('filePick').addEventListener('change', handleFileSelect, false);
 
-    //undo
-    $("#undo").click(function(){
-        for(var i=0; i<=differences[differences.length-1]; i++){
-        clickX.pop();
-        clickY.pop();
-        clickDrag.pop();
-        clickColor.pop();
-        }
-        redraw();
-        differences.pop();
-    });
-    
 });
 
 function redraw(){
-    context.save();
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    context.drawImage(imageObj, 0, 0, width, height);
+    context.drawImage(imageObj, 0, 0, canvasWidth,canvasHeight);
+    
     //drawing
     context.lineJoin = "round";
-    context.lineWidth = 5;    
+    context.lineWidth = 5;
+
     for(var i=0; i < clickX.length; i++) {		
         context.beginPath();
         if(clickDrag[i] && i){
@@ -171,14 +104,12 @@ function redraw(){
         }
         context.lineTo(clickX[i], clickY[i]);
         context.closePath();
-        context.strokeStyle = clickColor[i];        
+        context.strokeStyle = clickColor[i];
         context.stroke();
-    }    
-    context.restore();
-    //drawing textbox
+    }
     context.fillStyle = '#333';
     if(textUsed){
-        context.fillRect(0, textYLoc-20, width, 30);  
+        context.fillRect(0, textYLoc-20, 480, 30);  
     }
     
     //typing
@@ -186,14 +117,16 @@ function redraw(){
     context.fillStyle = "#ffffff";
     context.textAlign = 'center';
     var x = canvas.width / 2;
-    context.fillText(textToFill, x, textYLoc+2, 470);
+    context.fillText(textToFill, x, textYLoc, 470);
     
     
+    
+
 }
 
 function clearCanvas()
 {
-	context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+	context.clearRect(0, 0, canvasWidth, canvasHeight);
     clickX = [];
     clickY = [];
     clickDrag = [];
@@ -201,7 +134,6 @@ function clearCanvas()
     imageObj.src = "";
     document.getElementById('textToAdd').value='';
     textToFill = '';
-    restorePoints = [];
 }
 
 function addClick(x, y, dragging)
@@ -214,8 +146,7 @@ function addClick(x, y, dragging)
     }else if(document.getElementById('text').checked){
         textYLoc = y;
         textUsed = true;
-        $("#textToAdd").css("width", width);
-        $("#textToAdd").css("top", textYLoc+61);
+        
     }
 }
 
@@ -249,23 +180,37 @@ function handleFileSelect(evt) {
     }
   }
 
-function upload() {
-    alert("upload!");
-    var img = canvas.toDataURL("image/png").split(',')[1];
+/*function share(){
+
+    var img;
+    try {
+        img = canvas.toDataURL('image/jpeg', 0.9).split(',')[1];
+    } catch(e) {
+        img = canvas.toDataURL().split(',')[1];
+    }
+    var w = window.open();
+    w.document.write('Uploading to imgur.com...');
     $.ajax({
-        url: 'https://api.imgur.com/3/image',
+        url: 'https://api.imgur.com/3/upload.json',
         type: 'POST',
-        headers: { "Authorization": "Client-ID bd9e5b076b91742" },
-        dataType: 'json',
+        headers: {
+            Authorization: 'Client-ID cc01e3195c1adc2'
+        },
         data: {
+            type: 'base64',
+            name: 'neon.jpg',
+            title: 'Nebula',
+            description: 'Made using http://29a.ch/sandbox/2011/neonflames/',
             image: img
         },
-        success: function (data) {
-            var url = 'http://imgur.com/' + data.data.id + '?tags';
-            $("<a>").html(url).attr("href", url).appendTo($("#container"));
-        },
-        error: function (response) {
-            console.log(response);
-        }
+        dataType: 'json'
+    }).success(function(data) {
+        var url = 'http://imgur.com/' + data.data.id + '?tags';
+        _gaq.push(['_trackEvent', 'neonflames', 'share', url]);
+        w.location.href = url;
+    }).error(function() {
+        alert('Could not reach api.imgur.com. Sorry :(');
+        w.close();
+        _gaq.push(['_trackEvent', 'neonflames', 'share', 'fail']);
     });
-}
+}*/
